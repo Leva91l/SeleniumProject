fe7c4af1de9b004484502658a0ad6b746e11b970
Николай <i@nik-levchenko.ru>
2024-12-23 23:12:15 +0700

    Первое задание

---
+++ lesson_1/lesson_1.py
@@ -0,0 +1,33 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+@pytest.fixture(autouse=True)
+def driver(request):
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    yield driver
+
+
+class TestSteamLoginForm:
+
+    URL_PAGE = "https://store.steampowered.com/"
+    HOME_PAGE_LOGIN_BUTTON = ("xpath", "//*[@id='global_action_menu']//a[contains(text(), 'войти')]")
+    LOGIN_FIELD = ("xpath", "//input[contains(@class, '_2GBWeup5cttgbTw8FM3tfx') and contains(@type, 'text')]")
+    PASSWORD_FIELD = ("xpath", "//input[contains(@class, '_2GBWeup5cttgbTw8FM3tfx') and contains(@type, 'password')]")
+    SIGN_IN_BUTTON = ("xpath", "//button[@type='submit']")
+    LOADING_BUTTON = (
+    "xpath", "//*[contains(@class, 'DjSvCZoKKfoNSmarsEcTS') or contains(@class, '_2NVQcOnbtdGIu9O-mB9-YE')]")
+    WRONG_LOGIN_TEXT = ("xpath", "//*[@class='_1W_6HXiG4JJ0By1qN_0fGZ']")
+
+    def test_login_steam(self, driver):
+        wait = WebDriverWait(driver, 10)
+        driver.get(self.URL_PAGE)
+        driver.find_element(*self.HOME_PAGE_LOGIN_BUTTON).click()
+        wait.until(EC.visibility_of_element_located(self.LOGIN_FIELD)).send_keys('dskjbvid')
+        driver.find_element(*self.PASSWORD_FIELD).send_keys('<PASSWORD>')
+        driver.find_element(*self.SIGN_IN_BUTTON).click()
+        assert wait.until(EC.visibility_of_element_located(self.LOADING_BUTTON)).is_displayed()
+        assert wait.until(EC.visibility_of_element_located(self.WRONG_LOGIN_TEXT)).is_displayed()
